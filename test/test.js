56ee105d8db0b3a7364c9465071ee3d46ae8db5d
Felix Zapata <felix.zapata@extern.isban.com>
2015-06-12 10:13:42 +0200

    test(test.js): it creates the test when it passes the a11y validation and when not

--- test/test.js
+++ test/test.js
@@ -1,21 +1,39 @@
 'use strict';
 var assert = require('assert');
 var gutil = require('gulp-util');
-var axeCore = require('./');
+var axeCore = require('../');
 
-it('should ', function (cb) {
+it('should pass the a11y validation', function (cb) {
 	var stream = axeCore();
 
-	stream.on('data', function (file) {
-		assert.strictEqual(file.contents.toString(), 'unicorns');
+	stream.on('data', function (results) {
+		assert.equal(0, results.violation.length);
+
 	});
 
 	stream.on('end', cb);
 
 	stream.write(new gutil.File({
 		base: __dirname,
-		path: __dirname + '/file.ext',
-		contents: new Buffer('unicorns')
+		path: __dirname + '/test/fixtures/working.html'
+	}));
+
+	stream.end();
+});
+
+
+it('should not pass the a11y validation', function (cb) {
+	var stream = axeCore();
+
+	stream.on('data', function (results) {
+		assert.not.equal(0, results.violation.length);
+	});
+
+	stream.on('end', cb);
+
+	stream.write(new gutil.File({
+		base: __dirname,
+		path: __dirname + '/test/fixtures/broken.html'
 	}));
 
 	stream.end();
