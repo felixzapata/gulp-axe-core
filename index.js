e5bb5326e4302159db3b8e9bd10bee11bb1f910d
Felix Zapata <fzberlinches@gmail.com>
2016-08-07 11:22:19 +0200

    feat(index.js): change how to call to axe-core

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var gutil = require('gulp-util');
 var vm = require('vm');
 var fs = require('fs');
 var through = require('through2');
-var cheerio = require("cheerio");
+var cheerio = require('cheerio');
 var DOMParser = require('xmldom').DOMParser;
 var context;
 //var axe = require(__dirname + '/node_modules/axe-core/axe.min.js');
+var axe = require('axe-core');
 
-vm.runInThisContext(fs.readFileSync(__dirname + '/node_modules/axe-core/axe.js'));
+//vm.runInThisContext(fs.readFileSync(__dirname + '/node_modules/axe-core/axe.js'));
+
+//vm.runInThisContext(axe);
 
 module.exports = function (options) {
 	/*if (!options.foo) {
@@ -29,9 +32,19 @@ module.exports = function (options) {
 		try {
 
 			//context = cheerio.load(file.contents.toString());
+			//context = file.contents.toString();
 			context = new DOMParser().parseFromString(file.contents.toString(), 'text/html');
-			file.contents = new Buffer(axe.a11yCheck(context.firstChild, options));
-			this.push(file);
+
+			//file.contents = new Buffer(axe.a11yCheck(context.firstChild, options));
+			//this.push(file);
+
+			//console.log(context.documentElement.attributes['0'].ownerDocument)
+
+			//console.log(context);
+
+			axe.a11yCheck(context, null, function(result) {
+				console.log(result);
+			});
 
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-axe-core', err));
