99de4128d170f1d4930217dcf1e712ac77d1aaee
Felix Zapata <fzberlinches@gmail.com>
2016-08-08 19:53:11 +0200

    feat(index.js): use webdriver to call the API

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 'use strict';
 var gutil = require('gulp-util');
-var vm = require('vm');
-var fs = require('fs');
 var through = require('through2');
-var cheerio = require('cheerio');
-var DOMParser = require('xmldom').DOMParser;
-var context;
-//var axe = require(__dirname + '/node_modules/axe-core/axe.min.js');
-var axe = require('axe-core');
+var AxeBuilder = require('axe-webdriverjs');
+var WebDriver = require('selenium-webdriver');
+var PLUGIN_NAME = 'gulp-axe-core';
 
-//vm.runInThisContext(fs.readFileSync(__dirname + '/node_modules/axe-core/axe.js'));
-
-//vm.runInThisContext(axe);
+var driver = new WebDriver.Builder()
+  .forBrowser('firefox')
+  .build();
 
 module.exports = function (options) {
-	/*if (!options.foo) {
-		throw new gutil.PluginError('gulp-axe-core', '`foo` required');
-	}*/
-	
 
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
@@ -25,29 +17,23 @@ module.exports = function (options) {
 			return;
 		}
 		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-axe-core', 'Streaming not supported'));
+			cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
 			return;
 		}
 
 		try {
 
-			//context = cheerio.load(file.contents.toString());
-			//context = file.contents.toString();
-			context = new DOMParser().parseFromString(file.contents.toString(), 'text/html');
-
-			//file.contents = new Buffer(axe.a11yCheck(context.firstChild, options));
-			//this.push(file);
-
-			//console.log(context.documentElement.attributes['0'].ownerDocument)
-
-			//console.log(context);
-
-			axe.a11yCheck(context, null, function(result) {
-				console.log(result);
-			});
+			driver
+				.get(file.path)
+				.then(function () {
+					AxeBuilder(driver)
+						.analyze(function (results) {
+							console.log(results);
+						});
+				});
 
 		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-axe-core', err));
+			this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
 		}
 
 		cb();
