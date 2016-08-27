a013569643fd76482a44fc28ed49f8171485ec49
Felix Zapata <fzberlinches@gmail.com>
2016-08-27 12:00:40 +0200

    feat(ESLint): replace JSHint with ESLint

--- tasks/mocha.js
+++ tasks/mocha.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('test', ['jshint'], function () {
+gulp.task('test', ['lint'], function () {
     return gulp.src('test.js', {cwd: './test', read: false})
         .pipe($.mocha());
 });
